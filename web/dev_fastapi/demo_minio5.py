7c843e7574f7011db88347edae9bf0da4562d394
wwstory <1018472917@qq.com>
2022-01-23 18:02:11 +0800

    add example: get file content

---
+++ web/dev_fastapi/demo_minio5.py
@@ -0,0 +1,24 @@
+# 引入MinIO包。
+from minio import Minio
+from minio.error import InvalidResponseError
+from fastapi import FastAPI
+import json
+
+
+app = FastAPI()
+
+# 使用endpoint、access key和secret key来初始化minioClient对象。
+minioClient = Minio('play.min.io',
+                    access_key='Q3AM3UQ867SPQQA43P2F',
+                    secret_key='zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG',
+                    secure=True)
+
+
+@app.get('/')
+async def get_content():
+    try:
+        obj = minioClient.get_object('wwbucket', 'tmp.json')
+    except InvalidResponseError as err:
+        print(err)
+    # return json.loads(obj.data.decode())
+    return json.loads(obj.data)             # json直接解析二进制数据
